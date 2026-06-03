64af8135de8aa58357fc35117013b73ae77ec5da
positive-builder2 <lalithadithyasarabu@gmail.com>
2026-06-03 13:25:44 +0530

    fixes for arcjet

--- src/ws/server.js
+++ src/ws/server.js
@@ -18,24 +18,41 @@ function broadcast(wss,payload){
 export function attachWebSocketServer(server){
     const wss = new WebSocketServer({server,path:'/ws',maxPayload:1024*1024});
 
-    wss.on('connection',async (socket,req)=>{
+
+    server.on('upgrade',async (req,socket,head)=>{
+        const {pathname} = new URL(req.URL,`http://${req.headers.host}`);
+
+        if(pathname !== 'ws'){
+            return;
+        }
+
         if(wsArcjet){
             try{
                 const decision = await wsArcjet.protect(req);
 
                 if(decision.isDenied()){
-                    const code = decision.reason.isRateLimit()?1013:1008;
-                    const reason = decision.reason.isRateLimit()?'Rate limit exceeded':'Access denied';
-
-                    socket.close(code,reason);
+                    if(decision.reason.isRateLimit()){
+                        socket.write('HTTP/1.1 429 Too many Requests\r\n\r\n');
+                    } else{
+                        socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
+                    }
+                    socket.destroy();
                     return;
                 }
             } catch(e){
-                console.error('WS connection error',e);
-                socket.close(1011,'Server security Error');
+                console.error('WS upgrade protection error',e);
+                socket.write('HTTP/1.1 500 INternal Server Error\r\n\r\n');
+                socket.destroy();
                 return;
             }
         }
+
+        wss.handleUpgrade(req,socket,head,(ws)=>{
+            wss.emit('connection',ws,req);
+        });
+
+    })
+    wss.on('connection',async (socket,req)=>{
         socket.isAlive = true;
         socket.on('pong',()=>{socket.isAlive = true;})
 
